04cb888d67c591ef9567ff544ccaa869c9aef1c9
Joshua <jesus.angel.carball.santaclara@gmail.com>
2026-02-10 23:33:44 +0100

    refactor: Update build system, move esbuild configuration, add gitignore, and introduce project documentation.

--- esbuild.config.mjs
+++
@@ -1,8 +0,0 @@
-import { buildPlugin } from '../../libs/obsidian-plugin/esbuild.config.mjs';
-
-buildPlugin({
-  pluginId: 'elo-obsidian-google-contacts-plugin',
-}).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
